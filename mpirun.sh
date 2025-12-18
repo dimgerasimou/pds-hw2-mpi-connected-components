b575b48bbc0fdaa0ca759d2d7833bf1bc6e8f6cf
Dimitris Gerasimou <gerasimou-gh@posteo.gr>
2025-12-18 20:32:33 +0200

    [Refactor] Update mpirun to optimze thread affinity

--- mpirun.sh
+++ mpirun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=rome
 #SBATCH --time=00:10:00
-#SBATCH --job-name=Connected-Components-Detection
+#SBATCH --job-name=Connected-Components-Detection-OpenMPI+OpenMP
 #SBATCH --output=becnhmarks/rome-8-16-%j.json
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=1
@@ -12,8 +12,11 @@ module load gcc openmpi
 mkdir -p benchmarks
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_PROC_BIND=close
+export OMP_PLACES=cores
+export OMP_DYNAMIC=false
 
 export RETRIES=10
 export FILEPATH=$SCRATCH/data/bin/com-Friendster.bin
 
-srun ./bin/connected_components_mpi -n $RETRIES $FILEPATH
+srun --cpu-bind=cores ./bin/connected_components_mpi -n $RETRIES $FILEPATH
