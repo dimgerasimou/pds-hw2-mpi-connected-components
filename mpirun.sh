40ecb52f965adb2870687cb6e20ad5237533b725
Dimitris Gerasimou <gerasimou-gh@posteo.gr>
2025-12-18 20:07:43 +0200

    [Refactor] Update mpirun script to match configuration

--- mpirun.sh
+++ mpirun.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-#SBATCH --partition=batch
-#SBATCH --time=1:00:00
+#SBATCH --partition=rome
+#SBATCH --time=00:10:00
 #SBATCH --job-name=Connected-Components-Detection
+#SBATCH --output=becnhmarks/rome-8-16-%j.json
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 
 module load gcc openmpi
 
+mkdir -p benchmarks
+
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun ./bin/connected_components_mpi
+export RETRIES=10
+export FILEPATH=$SCRATCH/data/bin/com-Friendster.bin
+
+srun ./bin/connected_components_mpi -n $RETRIES $FILEPATH
